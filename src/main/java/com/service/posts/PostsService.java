894a56254f1b88f8a24fc94502baab06e1a6c5c5
johnnyhong33 <johnnyhong33@gmail.com>
2021-02-10 16:43:29 +0900

    git ignore 등록

---
+++ src/main/java/com/service/posts/PostsService.java
@@ -0,0 +1,12 @@
+package com.service.posts;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+
+
+public class PostsService {
+}
---
+++ src/main/java/com/web/dto/PostsSaveRequestDto.java
@@ -0,0 +1,4 @@
+package com.web.dto;
+
+public class PostsSaveRequestDto {
+}
